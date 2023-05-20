da9cf99bf5e22e251059bb43d311b4c81aa86cbd
sthiven <sthiven.correia@global.ntt>
2023-05-20 02:00:59 -0300

    :construction: changes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import jwt from '@fastify/jwt'
 import 'dotenv/config'
 import { memoriesRoutes } from './routes/memories';
 import { authRoutes } from './routes/auth';
+import axios from 'axios';
 
 const app = fastify();
 
@@ -14,36 +15,13 @@ app.register(cors, {
 app.register(jwt, {
     secret: 'spacetime',
 })
-app.get('/hello', async () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer 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");
-
-    var raw = JSON.stringify({
-        "model": "text-davinci-003",
-        "prompt": "Diga olá para mim com uma frase de boas vindas em português",
-        "max_tokens": 50,
-        "temperature": 1.4
-    });
-    const response = await fetch("https://api.openai.com/v1/completions", {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    })
-        .then((response: any) => {
-            return response.json()
-        })
-        .catch(error => error)
-    response.object
-    return response.choices[0].text
-})
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app.listen({
     port: 3333,
+    host: '0.0.0.0',
 }).then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333 🚀')
 });
